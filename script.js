077ed68071a526c9c1f7c8d2801f7e18cab633cf
YashMahale1702 <yashmahale1702@gmail.com>
2022-03-21 01:44:17 +0530

    Minor changes

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
 particlesJS('particles-js', {
 	particles: {
 		number: {
-			value: 355,
+			value: 350,
 			density: {
 				enable: true,
-				value_area: 789.1476416322727
+				value_area: 800
 			}
 		},
 		color: {
-			value: '#ffffff'
+			value: '#FDF6EC'
 		},
 		shape: {
 			type: 'circle',
 			stroke: {
 				width: 0,
-				color: '#000000'
+				color: '#FDF6EC'
 			},
 			polygon: {
 				nb_sides: 5
-			},
-			image: {
-				src: 'img/github.svg',
-				width: 100,
-				height: 100
 			}
+			// image: {
+			// 	src: 'img/github.svg',
+			// 	width: 100,
+			// 	height: 100
+			// }
 		},
 		opacity: {
 			value: 0.48927153781200905,
@@ -99,10 +99,10 @@ particlesJS('particles-js', {
 				duration: 0.4
 			},
 			push: {
-				particles_nb: 4
+				particles_nb: 1
 			},
 			remove: {
-				particles_nb: 2
+				particles_nb: 1
 			}
 		}
 	},
